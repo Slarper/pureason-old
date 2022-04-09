0976709506ca3a3f2133efcd6050c52aff7564ff
SpaceTeleport <naivesimple@outlook.com>
2022-04-10 01:28:33 +0800

    add comments PullableSpellItem

--- src/main/java/slarper/pureason/item/PullableSpellItem.java
+++ src/main/java/slarper/pureason/item/PullableSpellItem.java
@@ -5,6 +5,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.UseAction;
 import net.minecraft.world.World;
 
 public class PullableSpellItem extends Item {
@@ -12,6 +13,14 @@ public class PullableSpellItem extends Item {
         super(settings);
     }
 
+    // body movement like drawing a bow (won't zoom in on the camera
+    @Override
+    public UseAction getUseAction(ItemStack stack) {
+        return UseAction.BOW;
+    }
+
+
+    // you can draw a bow for up to 72000 ticks
     @Override
     public int getMaxUseTime(ItemStack stack) {
         return 72000;
@@ -20,6 +29,8 @@ public class PullableSpellItem extends Item {
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
 
+        // must call this method here to allow pulling.
+        // because it will set entity.itemUseTimeLeft to stack.getMaxUseTime()
         user.setCurrentHand(hand);
 
         return super.use(world, user, hand);
